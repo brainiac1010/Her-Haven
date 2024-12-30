e6851bd86a1b49d1b977ba5cdd33c5423499fb2d
Arman Hossain <armanmahim00@gmail.com>
2024-12-30 11:05:07 +0600

    categorie and hero section added

---
+++ src/components/pages/Shop/TrendingProducts.jsx
@@ -0,0 +1,13 @@
+
+
+const TrendingProducts = () => {
+    return (
+        <section className="section__container product__container">
+            <h2 className="section__header">Tranding Products</h2>
+            <p className="section__subheader mb-12">Discover the hottest trends! Elevate your style with our curated collection of women's fashion products.</p>
+
+        </section>
+    )
+}
+
+export default TrendingProducts
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 
+import TrendingProducts from '../Shop/TrendingProducts'
 import Banner from './Banner'
 import Categories from './Categories'
+import HeroSection from './HeroSection'
 
 const Home = () => {
   return (
  <>
  <Banner></Banner>
  <Categories></Categories>
+ <HeroSection></HeroSection>
+ <TrendingProducts></TrendingProducts>
  </>
   )
 }
