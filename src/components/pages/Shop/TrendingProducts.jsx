32ea2c65ea9cdc9fc464d8698431c94b22915e08
Arman Hossain <armanmahim00@gmail.com>
2024-12-31 21:52:19 +0600

    Load more button added , and mb-12 probem fix

--- src/components/pages/Shop/TrendingProducts.jsx
+++ src/components/pages/Shop/TrendingProducts.jsx
@@ -12,13 +12,23 @@ const TrendingProducts = () => {
     return (
         <section className="section__container product__container">
             <h2 className="section__header">Tranding Products</h2>
-            <p className="section__subheader mb-12">Discover the hottest trends! Elevate your style with our curated collection of women's fashion products.</p>
+            <p className="section__subheader ">
+                Discover the hottest trends! Elevate your style with our curated collection of women's fashion products.
+            </p>
 
 
             {/* Products Cards */}
-
-            <ProductCards products={products}></ProductCards>
-
+            <div className="mt-12">
+                <ProductCards products={products.slice(0,visibleProducts)} ></ProductCards>
+            </div>
+            {/* load more product btn */}
+            <div className="product__btn">
+                {
+visibleProducts< products.length &&(
+    <button className="btn" onClick={loadMoreProducts}>Load More</button>
+)
+                }
+            </div>
         </section>
     )
 }
