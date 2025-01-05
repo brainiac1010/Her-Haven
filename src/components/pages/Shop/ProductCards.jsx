0f6cd08294a72c22e72a7a7e8a0bee0799659b76
Arman Hossain <armanmahim00@gmail.com>
2025-01-05 19:11:25 +0600

    setup Redux Toolkit , and fix the  add to cart  number

--- src/components/pages/Shop/ProductCards.jsx
+++ src/components/pages/Shop/ProductCards.jsx
@@ -1,7 +1,16 @@
 import { Link } from 'react-router-dom';
 import RatingStar from '../../RatingStar';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../../redux/features/cart/cartSlice';
 
 const ProductCards = ({ products }) => {
+
+    const dispatch = useDispatch()
+
+    const handleAddToCart = (product) => {
+
+        dispatch(addToCart(product))
+    }
     console.log(products);
 
     return (
@@ -18,7 +27,11 @@ const ProductCards = ({ products }) => {
                                 />
                             </Link>
                             <div className='hover:block absolute top-3 right-3'>
-                                <button>
+                                <button onClick={(e) => {
+
+                                    e.stopPropagation();
+                                    handleAddToCart(product)
+                                }}>
                                     <i className="ri-shopping-cart-2-line bg-primary p-1.5 text-white hover:bg-primary-dark"></i>
                                 </button>
                             </div>
@@ -28,7 +41,7 @@ const ProductCards = ({ products }) => {
                         <div className='product__card__content'>
                             <h4>{product.name}</h4>
                             <p>${product.price} {product.oldPrice ? <s>${product?.oldPrice}</s> : null}</p>
-                        <RatingStar rating={product.rating}></RatingStar>
+                            <RatingStar rating={product.rating}></RatingStar>
                         </div>
                     </div>
                 ))
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './rourers/router.jsx'
+import router from './routers/router.jsx'
 import 'remixicon/fonts/remixicon.css'
+import { store } from '../src/redux/store.js'
+import { Provider } from 'react-redux'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store} >
     <RouterProvider router={router}></RouterProvider>
-  </StrictMode>,
+  </Provider>,
 )
