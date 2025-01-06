b3d4c074dd2e979c1eea991803755caae2af2680
Arman Hossain <armanmahim00@gmail.com>
2025-01-06 19:20:37 +0600

    order summery added

--- src/components/pages/Shop/OrderSummary.jsx
+++ src/components/pages/Shop/OrderSummary.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+
+import { useSelector } from 'react-redux'
+
 
 const OrderSummary = () => {
-  return (
-    <div>OrderSummary</div>
-  )
+
+    const products = useSelector((store) => store.cart.products);
+
+    console.log(useSelector((store) => store.cart))
+
+    const { tax, taxRate, totalPrice, grandTotal, selectedItems } = useSelector((store) => store.cart)
+
+    return (
+        <div className='bg-primary-light mt-5 rounded text-base '>
+            <div className='px-6 py-4 space-y-5'>
+                <h2 className='text-xl  text-text-dark'>Order Summary</h2>
+                <p className='text-text-dark mt-2'>Selected Items: {selectedItems}</p>
+                <p>Total Price: ${totalPrice.toFixed(2)}</p>
+                <p>Tax: ({taxRate*100}%): ${tax.toFixed(2)}</p>
+                <h3 className='font-bold'>Grand Total: ${grandTotal.toFixed(2)}</h3>
+                <div className=' px-4 mb-6 '>
+                    <button className='bg-red-500 px-3 py-1.5 text-amber-50 mt-2 rounded-md flex justify-between items-center mb-4'> <span className='mr-2 '> Clear Cart </span> <i className="ri-delete-bin-fill"></i></button>
+                    <button className='bg-green-500 px-3 py-1.5 text-amber-50 mt-2 rounded-md flex justify-between items-center mb-4'> <span className='mr-2'> Proceed Checkout</span><i className="ri-bank-card-line"></i></button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default OrderSummary
